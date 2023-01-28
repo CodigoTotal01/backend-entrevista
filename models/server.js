37fa71c8fcca274c5ab7802008520c7e1616fa92
Palacios Tarrillo Christian <85316618+PalaciosTarrilloChristian@users.noreply.github.com>
2023-01-27 23:00:59 -0500

    base de datos online

---
+++ models/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+require('dotenv').config(); //permite acceder a las constates en el archivo .env
+let cors = require('cors') //*
+const { dbConnection } = require('../database/config');
+
+
+class Server{
+    constructor() {
+        this.app = express()
+        this.port = process.env.PORT;
+        this.conectarDB();
+        this.middlewares();
+    }
+
+    async conectarDB(){
+        await dbConnection();
+    }
+
+    middlewares(){
+        this.app.use(express.static('public'));
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+
+    listen(){
+        this.app.listen(this.port, ()=> {
+            console.log('Servidor corriendo');
+        })
+    }
+
+}
+
+module.exports = Server;
